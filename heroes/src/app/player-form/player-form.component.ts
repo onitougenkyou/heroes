1b576d65c770ab7b7941ba109421baf49bd9fb77
DESKTOP-9R8UI45\death-pc-doudou <onitougenkyou@gmail.com>
2018-03-30 11:51:58 +0200

    modification du style global (en cours)

--- heroes/src/app/player-form/player-form.component.ts
+++ heroes/src/app/player-form/player-form.component.ts
@@ -9,7 +9,7 @@ import { Inventaire } from '../personnage/inventaire';
 import { Player } from '../personnage/player';
 import { Monney } from '../personnage/monney';
 import { Sell } from '../personnage/sell';
-
+import { Attributes } from '../personnage/attributes';
 
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
@@ -17,7 +17,7 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 @Component({
   selector: 'app-player-form',
   templateUrl: './player-form.component.html',
-  styleUrls: ['./player-form.component.sass']
+  styleUrls: ['./player-form.component.scss']
 })
 export class PlayerFormComponent implements OnInit {
 
@@ -25,12 +25,45 @@ export class PlayerFormComponent implements OnInit {
   Repdata;
   valbutton = "Save";
   playerDefine;
+  userChoice;
+  playerArray = [];
+  playerName;
 
 
   ngOnInit() {
     this.newService.getPlayer().subscribe(data => this.Repdata = data);
+    this.definePlayerClass();
   }
 
+  /**
+   * Appel les différentes méthodes de création de personnage pour remplir le tableau arrayOfPlayerClass
+   */
+  definePlayerClass() {
+    this.defineBarbare();
+  }
+
+  /**
+   * Définit la classe barbare
+   */
+  defineBarbare() {
+    var weaponValue = new Monney(10);
+    var barbareWeapon = new Weapon("Hache à deux mains", weaponValue, 5, 1);
+    let defValue = new Monney(0);
+    var barbareDefense = new Defense("", defValue, 0, 1);
+    var sellObject = new Sell("bibelots", new Monney(5));
+    var barbareSell: Array<Sell> = [sellObject, sellObject];
+    var barbareInventaire = new Inventaire(barbareWeapon, barbareDefense, new Monney(50), barbareSell);
+    var barbareAttributes = new Attributes(150, 10, 4, 5, 5, 6);
+    var barbareClass = new Class('barbare', barbareAttributes);
+    var barbare = new Player('', 1, 0, barbareClass, barbareInventaire, true);
+
+    this.playerArray.push(barbare);
+    console.log(barbare);
+  }
+
+  /**
+   * Permet la sauvegarde du joueur 
+   */
   onSave = function (player, isValid: boolean) {
     player.mode = this.valbutton;
     this.playerDefine = {
@@ -47,7 +80,7 @@ export class PlayerFormComponent implements OnInit {
     }
 
     console.log('player', player, 'this.playerDefine', this.playerDefine)
-    
+
     this.newService.savePlayer(this.playerDefine)
       .subscribe(data => {
         this.ngOnInit();
@@ -64,7 +97,7 @@ export class PlayerFormComponent implements OnInit {
     this.strenght = kk.class.attributes.strenght;
     this.valbutton = "Edit";
     this.mode = this.valbutton;
-    console.log("THIS ID IS ",this.id, "KKKKK ==>", kk);
+    console.log("THIS ID IS ", this.id, "KKKKK ==>", kk);
   }
 
   delete = function (id) {
